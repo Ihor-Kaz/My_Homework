04f75d81fc3a6131da26516e1fe8f8f4a962948b
Olena Morozenko <helya.ua@gmail.com>
2023-06-11 13:23:46 +0300

    modified code for homework1

--- homework1_str_format.py
+++ homework1_str_format.py
@@ -6,12 +6,17 @@
 # to check your new created python module for possible linter errors
 # try to run code from pycharm/command line
 
-name1 = 'ø'
-name2 = 'å'
-name3 = 'Æ'
-norway_text = 'Automatisering akselererer %syeblikket da roboter vil erobre planeten v%sr. (%s)'% ('ø', 'å', 'Æ')
-norway_text1 = 'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ')
-norway_text2 = f'Automatisering akselererer {name1}yeblikket da roboter vil erobre planeten v{name2}r. ({name3})'
-print(norway_text)
-print(norway_text1)
-print(norway_text2)
+name1, name2, name3 = 'ø', 'å', 'Æ'
+print(('Automatisering akselererer %syeblikket ' +
+       'da roboter vil erobre planeten v%sr. (%s)'
+       ) % (name1, name2, name3),
+      )
+print(('Automatisering akselererer {0}yeblikket ' +
+       'da roboter vil erobre ' +
+       'planeten v{1}r. ({2})'
+       ).format(name1, name2, name3),
+      )
+print((f'Automatisering akselererer {name1}yeblikket ' +
+       f'da roboter vil erobre planeten v{name2}r. ({name3})'
+       ),
+      )
