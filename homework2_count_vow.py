814b15d4fb00a91ab790ca26a7579438e952dd14
Olena Morozenko <helya.ua@gmail.com>
2023-06-19 18:49:50 +0300

    reworked homework 2 according to comments and deleted homework1 from pull request - 2nd chance

--- homework2_count_vow.py
+++ homework2_count_vow.py
@@ -1,10 +1,10 @@
 """Home-task02.
 
 Task description:
-The English language has five vowels: A, E, I, O, and U
-Please count each vowel occurence in text bellow
-( sum of lower and capital cases)
-and write output as table smth like this
+    The English language has five vowels: A, E, I, O, and U
+    Please count each vowel occurence in text bellow
+    ( sum of lower and capital cases)
+    and write output as table smth like this
 -----------------
 | vowel | count |
 -----------------
@@ -12,13 +12,13 @@ and write output as table smth like this
 |   e   |  23   |
 ...
 -----------------
-then modify text where each vowel replaced with
-A->À;  a->à ; E-> É ; e->é; I->Í , i->í ; O->Ó ; o->ó; U->Ú; u->ú
-ex. "Í wàndéréd lónély...."   and print it
+    then modify text where each vowel replaced with
+    A->À;  a->à ; E-> É ; e->é; I->Í , i->í ; O->Ó ; o->ó; U->Ú; u->ú
+    ex. "Í wàndéréd lónély...."   and print it
 """
-sep_num = 27
-a = 'AaEeIiOoUu'
-b = 'ÀàÉéÍíÓóÚú'
+SEP_NUM = 27
+eng_vowels = 'AaEeIiOoUu'
+replaced_vowels = 'ÀàÉéÍíÓóÚú'
 
 poem_text = """I wandered lonely as a cloud
 That floats on high o'er vales and hills,
@@ -34,21 +34,22 @@ Along the margin of a bay:
 Ten thousand saw I at a glance,
 Tossing their heads in sprightly dance."""
 
-print('-' * sep_num)
-print(f"| {'vowel':^10} | {'count':^10} |")
-print('-' * sep_num)
-poem = poem_text.lower()
-count_a = poem.count('a')
-print(f"| {'a':^10} | {count_a:^10} |")
-count_e = poem.count('e')
-print(f"| {'e':^10} | {count_e:^10} |")
-count_i = poem.count('i')
-print(f"| {'i':^10} | {count_i:^10} |")
-count_o = poem.count('o')
-print(f"| {'o':^10} | {count_o:^10} |")
-count_u = poem.count('u')
-print(f"| {'u':^10} | {count_u:^10} |")
-print('-' * sep_num)
+poem_lower = poem_text.lower()  # change all symbols to lowercase
+count_a = poem_lower.count('a')
+count_e = poem_lower.count('e')
+count_i = poem_lower.count('i')
+count_o = poem_lower.count('o')
+count_u = poem_lower.count('u')
 
-modified_text = str.maketrans(a, b)
+print('-' * SEP_NUM)
+print(f"| {'vowel':^10} | {'count':^10} |")
+print('-' * SEP_NUM)
+print(f"| {'a':^10} | {count_a:^10} |")
+print(f"| {'e':^10} | {count_e:^10} |")
+print(f"| {'i':^10} | {count_i:^10} |")
+print(f"| {'o':^10} | {count_o:^10} |")
+print(f"| {'u':^10} | {count_u:^10} |")
+print('-' * SEP_NUM)
+
+modified_text = str.maketrans(eng_vowels, replaced_vowels)
 print(poem_text.translate(modified_text))
