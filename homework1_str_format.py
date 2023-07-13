9aaa36b9ba32cc6f6d148d0da449d54555d6d7a5
Olena Morozenko <helya.ua@gmail.com>
2023-07-13 19:24:05 +0300

    added homework1

---
+++ homework1_str_format.py
@@ -0,0 +1,31 @@
+"""
+Lesson01 Task01.
+
+Descrition:
+    we have norway text in old style formatting
+    re-write the same text as:
+    #1 string with format() call
+    #2 f-string
+    use linter(https://github.com/wemake-services/wemake-python-styleguide)
+    to check your new created python module for possible linter errors
+    try to run code from pycharm/command line
+
+    norway_text = 'Automatisering akselererer %syeblikket da roboter
+    vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+"""
+
+norway_text = ('Automatisering akselererer %syeblikket da roboter vil'
+               ' erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+               )
+
+format_norway_text = ('Automatisering akselererer {0}yeblikket da roboter vil'
+                      ' erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ')
+                      )
+
+f_string_norway_text = (f'Automatisering akselererer {"ø"}yeblikket da roboter'
+                        f' vil erobre planeten v{"å"}r. ({"Æ"})'
+                        )
+
+print(norway_text)
+print(format_norway_text)
+print(f_string_norway_text)
